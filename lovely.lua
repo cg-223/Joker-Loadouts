5e1feeeeb2b948f813219ee4e2e1b34635511e76
cg223 <cg223celeste@gmail.com>
2025-09-20 22:20:19 -0400

    don't try to use NFS

--- lovely.lua
+++ lovely.lua
@@ -1,20 +1,12 @@
 JKRLDTS = {}
-local NFS = NFS
-if not NFS then
-    local succ, ret = pcall(require, "nativefs")
-    if succ then
-        NFS = ret
-    else
-        NFS = love.filesystem
-    end
-end
+local fs = love.filesystem
 
 
 local function find_jkrldts()
     local start_dir = "Mods"
-    local files = NFS.getDirectoryItems(start_dir)
+    local files = fs.getDirectoryItems(start_dir)
     for _, file in ipairs(files) do
-        if NFS.getInfo(start_dir.."/"..file.."/".."JKRLDTS_MARKER") then
+        if fs.getInfo(start_dir.."/"..file.."/".."JKRLDTS_MARKER") then
             return start_dir.."/"..file.."/"
         end
     end
@@ -32,5 +24,5 @@ local mod_path = find_jkrldts()
 
 for i, v in ipairs(JKRLDTS.modules) do
 ---@diagnostic disable-next-line: param-type-mismatch
-    load(NFS.read(mod_path.."modules/"..v..".lua"))()
+    load(fs.read(mod_path.."modules/"..v..".lua"))()
 end
